0d85c9c94b4acabf9e19d29a0120529c83b6ab70
Erwin2307-py <erwin.schimak@novogenia.com>
2025-02-24 08:53:00 +0100

    Delete modules/modul_b.py

--- modules/modul_b.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-def fenster_oeffnen():
-    st.subheader("Modul B")
-    st.write("Hier befindet sich die Logik/Funktionen für Modul B.")
-    st.write("Führe hier beliebige Operationen durch, zeige Diagramme an etc.")
