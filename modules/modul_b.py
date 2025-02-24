981a5a292391a24982a159999a99377f012c4b6a
Erwin2307-py <erwin.schimak@novogenia.com>
2025-02-24 08:39:44 +0100

    Add files via upload

---
+++ modules/modul_b.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+def fenster_oeffnen():
+    st.subheader("Modul B")
+    st.write("Hier befindet sich die Logik/Funktionen für Modul B.")
+    st.write("Führe hier beliebige Operationen durch, zeige Diagramme an etc.")
