ab6dced96bd3a6b43966f01c87051f1b3f1af9c4
Erwin2307-py <erwin.schimak@novogenia.com>
2025-03-04 10:37:21 +0100

    Update online_api_filter.py

---
+++ modules/online_api_filter.py
@@ -0,0 +1,6 @@
+# modules/online_api_filter.py
+import streamlit as st
+
+def module_online_api_filter():
+    st.header("Online API Filter Modul")
+    st.write("Hier kommt dein kombinierter Code hin.")
